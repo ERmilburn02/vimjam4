1c623f175cb77356c3b7575d62631a1b0195b36a
Eliza Milburn <ERmilburn02@outlook.com>
2023-09-08 19:52:33 +0100

    Sprites

--- src/assets.ts
+++ src/assets.ts
@@ -1,7 +1,18 @@
 import type { ResolverManifest } from "pixi.js";
 
 const manifest: ResolverManifest = {
-  bundles: [],
+  bundles: [
+    {
+      name: "shapes",
+      assets: {
+        circle: "sprites/Circle.png",
+        hexagon: "sprites/Hexagon.png",
+        pentagon: "sprites/Pentagon.png",
+        square: "sprites/Square.png",
+        triangle: "sprites/Triangle.png",
+      },
+    },
+  ],
 };
 
 export default manifest;
