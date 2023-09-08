8090a5a4abe9617b110113c3ac60c4c9a99096c6
Eliza Milburn <ERmilburn02@outlook.com>
2023-09-08 23:04:25 +0100

    Require button to start (to enable audio)

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,25 @@ import Manager from "@/Engine/Manager";
 // import DefaultLoadingScreen from "@/Engine/Scenes/DefaultLoadingScene";
 import DevMap from "@/Game/Scenes/DevMap";
 
-const canvas: HTMLCanvasElement = document.getElementById(
-  "game-canvas"
-) as HTMLCanvasElement;
+const div: HTMLElement | null = document.getElementById("game-content");
 
-Manager.initialize(1280, 720, 0x000000, canvas);
-Manager.changeScene(new DevMap());
+const button = document.createElement("button");
+button.innerText = "Start";
+button.style.border = "none";
+button.style.width = "10em";
+button.style.height = "3em";
+button.style.fontSize = "2em";
+div?.appendChild(button);
+
+const buttonEvent = () => {
+  div?.removeChild(button);
+  button.removeEventListener("click", buttonEvent);
+
+  const canvas = document.createElement("canvas");
+  div?.appendChild(canvas);
+
+  Manager.initialize(1280, 720, 0x000000, canvas);
+  Manager.changeScene(new DevMap());
+};
+
+button.addEventListener("click", buttonEvent);
